ce549bc32fbb59859d442ce02efab7a12e240476
Fakhri <mmmdov.fxri@bk.ru>
2022-12-26 21:52:56 +0400

    first step in authentication

--- starexclone/contact/models.py
+++ starexclone/contact/models.py
@@ -10,6 +10,10 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+    
+    class Meta:
+        verbose_name_plural='Ölkələr'
+
 
 
 class CountryContact(models.Model):
@@ -20,6 +24,10 @@ class CountryContact(models.Model):
 
     def __str__(self):
         return f'{self.location}'
+
+    
+    class Meta:
+        verbose_name_plural='Ölkə kontaktları'
     
 
 
@@ -33,6 +41,10 @@ class District(models.Model):
     def __str__(self):
         return self.name
 
+
+    class Meta:
+        verbose_name_plural='Rayonlar'
+
 class PointContact(models.Model):
     name=models.CharField(max_length=50,null=True)
     slug=models.SlugField(max_length=50,null=True)
@@ -43,6 +55,9 @@ class PointContact(models.Model):
 
     def __str__(self):
         return f'{self.location}'
+
+    class Meta:
+        verbose_name_plural='Məntəqələr'
 
 
 
